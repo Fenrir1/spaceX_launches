8f8907d31153230b5473763fd0e5115d964a0ed2
Fenrir1 <fenrir.new@gmail.com>
2019-07-25 00:03:16 +0500

    Fineshed

---
+++ public/precache-manifest.b468e6c579bd3275f0b9ce822c058325.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba4962e4320291d3471a496d261cddc9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cd6df30ba198ebfa65b3",
+    "url": "/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "453ef4aaaf1b3be0146f",
+    "url": "/static/js/2.f8f38f71.chunk.js"
+  },
+  {
+    "revision": "cd6df30ba198ebfa65b3",
+    "url": "/static/js/main.42e80dbc.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "664b811513519f7dbf78f000c4ae73e1",
+    "url": "/static/media/logo.664b8115.png"
+  }
+]);
